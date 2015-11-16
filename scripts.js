9659953fc0e6df7857f6701fb46e02e4a3bce7bd
eric-bone <therealeric@gmail.com>
2015-11-16 14:57:04 -0700

    canvas is the same size as the window now

--- scripts.js
+++ scripts.js
@@ -1,51 +1,15 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-// ctx.fillStyle = "green";
-// ctx.fillRect(10, 10, 100, 100);
+(function(){
+  // create a wrapper around native canvas element (with id="c")
+  var canvas = new fabric.Canvas('c');
 
+  window.addEventListener('resize', resizeCanvas, false);
 
+  function resizeCanvas() {
+    canvas.setHeight(window.innerHeight);
+    canvas.setWidth(window.innerWidth);
+    canvas.renderAll();
+  }
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+  resizeCanvas();
 
-var background = new Image();
-background.src = "http://showerthoughts.eboone.info/images/19.jpeg";
-// background.repeat = "no-repeat";
-// background.position = "center center";
-// background.attachment = "fixed";
-// background.size = "cover";
-
-background.onload = function(){
-  ctx.drawImage(background,0,0);
-}
-
-
-
-
-// below from https://jsfiddle.net/jaredwilli/qFuDr/
-// useing mootools
-// (function(){
-//   var canvas = document.getElementById('canvas');
-//   var context = canvas.getContext('2d');
-//
-//   window.addEventListener('resize', resizeCanvas, false);
-//
-//   function resizeCanvas() {
-//     canvas.width = window.innerWidth;
-//     canvas.height = window.innerHeight;
-//
-//     /* DRAW BELOW HERE */
-//     // ctx.fillStyle = "green";
-//     // ctx.fillRect(10, 10, 100, 100);
-//     /* DRAW ABOVE HERE */
-//     drawStuff();
-//   }
-//   resizeCanvas();
-//
-//   function drawStuff() {
-//     /* DRAW BELOW HERE */
-//     ctx.fillStyle = "green";
-//     ctx.fillRect(10, 10, 100, 100);
-//     /* DRAW ABOVE HERE */
-//   }
-// })();
+})();
