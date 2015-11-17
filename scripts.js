b94e4d955027b885cf0a62aa5ccdba40c4435ce8
eric-bone <therealeric@gmail.com>
2015-11-16 19:41:47 -0700

    EOD commit

--- scripts.js
+++ scripts.js
@@ -1,6 +1,11 @@
 (function(){
   // create a wrapper around native canvas element (with id="c")
-  var canvas = new fabric.Canvas('c');
+  var canvas = new fabric.StaticCanvas('c');
+
+  canvas.setBackgroundImage('images/19.jpeg', canvas.renderAll.bind(canvas), {
+    backgroundImageOpacity: 1,
+    backgroundImageStretch: false
+  });
 
   window.addEventListener('resize', resizeCanvas, false);
 
@@ -11,5 +16,17 @@
   }
 
   resizeCanvas();
+
+  var text = new fabric.Text('Hello World!', {
+    fill: 'rgb(120,120,0)',
+    left: 0,
+    top: 0,
+    fontFamily: 'Helvetica',
+    fontSize: 40,
+    shadow: 'black 2px 2px 2px'
+  });
+  canvas.add(text);
+
+  // canvas.centerObject(text);
 
 })();
