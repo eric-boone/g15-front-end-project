b9dc6e4951f0d567b7327297edb3fb281a5f3bdb
eric-bone <therealeric@gmail.com>
2015-11-18 11:29:53 -0700

    image centered

--- scripts.js
+++ scripts.js
@@ -6,23 +6,63 @@ function init() {
   demoCanvas.width = window.innerWidth;
   demoCanvas.height = window.innerHeight;
 
-  var grass = new createjs.Bitmap("images/19.jpeg").setTransform(
-    0, // x=0, The horizontal translation (x position) in pixels
-    0, //y=0, The vertical translation (y position) in pixels
-    0.5, //scaleX=1, The horizontal scale, as a percentage of 1
-    0.5, //scaleY=1, the vertical scale, as a percentage of 1
-    0, //rotation=0, The rotation, in degrees
-    0, //skewX=0, The horizontal skew factor
-    0, //skewY=0, The vertical skew factor
-    demoCanvas.width, //regX=0, The horizontal registration point in pixels
-    demoCanvas.height); //regY=0, The vertical registration point in pixels
+  var canvas = document.getElementById('demoCanvas');
+  var context = canvas.getContext('2d');
+  var bgImage = new Image();
+  bgImage.src = "images/19.jpeg";
+  bgImage.onload = function(){
+    context.save();
+    //matrix
+    // var widthRatio = canvas width / image width
+    // var heightRatio = canvas hieght / image height
+    // whicher ever is bigger send to scaleFactor
 
-    console.log(grass.x);
+    var widthRatio = canvas.width / bgImage.width;
+    var heightRatio = canvas.height / bgImage.height;
+    // console.log('hR'+heightRatio);
+    // console.log('wR'+widthRatio);
+    var scaleFactor = function(){
+      if (widthRatio >= heightRatio){
+        return widthRatio;
+      } else {
+        return heightRatio;
+      }
+    }
+    console.log(scaleFactor());
 
-  grass.image.onload = function() {
-    stage.update();
+    context.translate(canvas.width/2, canvas.height/2);
+    context.scale(scaleFactor, scaleFactor);
+    context.translate((-bgImage.width/2),(-bgImage.height)/2);
+
+
+    context.drawImage(bgImage,0,0);
+    context.restore();
   }
-  stage.addChild(grass);
+
+
+  // var grass = new createjs.Bitmap("images/19.jpeg")
+  //
+  //   var imageSize = grass.getBounds();
+  //
+  //   grass.setTransform(
+  //   demoCanvas.width / 2 - 6000 / 2, // x=0, The horizontal translation (x position) in pixels
+  //   demoCanvas.height / 2 - 4000 / 2, //y=0, The vertical translation (y position) in pixels
+  //   1, //scaleX=1, The horizontal scale, as a percentage of 1
+  //   1, //scaleY=1, the vertical scale, as a percentage of 1
+  //   0, //rotation=0, The rotation, in degrees
+  //   0, //skewX=0, The horizontal skew factor
+  //   0, //skewY=0, The vertical skew factor
+  //   0, //regX=0, The horizontal registration point in pixels
+  //   0); //regY=0, The vertical registration point in pixels
+  //
+  //   grass.scaleX
+  //
+  //   console.log(grass.x);
+  //
+  // grass.image.onload = function() {
+  //   stage.update();
+  // }
+  // stage.addChild(grass);
 
   window.addEventListener('resize', function(){
     console.log('hello');
